e4bc38a0cf754bf3fb9ed645051a4ad5b4d342f9
shannen <shannenflame25@gmail.com>
2026-03-15 00:37:26 +0000

    Initial Commit

---
+++ watersheep/watersheepApp.swift
@@ -0,0 +1,17 @@
+//
+//  watersheepApp.swift
+//  watersheep
+//
+//  Created by shannen immanuel on 15/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct watersheepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
